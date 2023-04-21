d62860c2e3078eb35de0e4f621d6fefa410b0662
Vikashsharma2003 <vikashsharma20103@gmail.com>
2023-04-21 18:23:20 +0530

    change code on dbconnect

--- connectionDB/connectDB.js
+++ connectionDB/connectDB.js
@@ -2,15 +2,31 @@ import mongoose from "mongoose";
 
 
 //  DATABASE CONNECTION 
-const connectDB =async (DB_URL) => {
-    try {
-        await mongoose.connect(DB_URL)
-        // console.log("db connect");
-    } catch (error) {
-        console.log(error);
-        
-    }
+const connectDB = async (DB_URL) => {
+    // try {
+    //     await mongoose.connect(DB_URL)
+    //     // console.log("db connect");
+    // } catch (error) {
+    //     console.log(error);
+
+    // }
+
+
+    mongoose.connect(DB_URL, {
+        useNewUrlParser: "true",
+        useUnifiedTopology: "true"
+    })
+    mongoose.connection.on("error", err => {
+
+        console.log("err", err)
+
+    })
+    mongoose.connection.on("connected", (err, res) => {
+
+        console.log("mongoose is connected")
+
+    })
 }
 
 
-export default connectDB ;
+export default connectDB;
