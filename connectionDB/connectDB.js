c8b1adb45deaf9531a628c0374279b6db8862fd1
Vikashsharma2003 <vikashsharma20103@gmail.com>
2023-04-21 18:06:43 +0530

    comment log in DBconnect

--- connectionDB/connectDB.js
+++ connectionDB/connectDB.js
@@ -5,9 +5,10 @@ import mongoose from "mongoose";
 const connectDB =async (DB_URL) => {
     try {
         await mongoose.connect(DB_URL)
-        console.log("db connect");
+        // console.log("db connect");
     } catch (error) {
         console.log(error);
+        
     }
 }
 
